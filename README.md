569cd8507a88b9f80227cbaf60a9ff7d924d452a
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-03-17 10:30:09 +0330

    Test configuration and build process is update to fix unit test issues

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Captcha module
 
+[![Build Status](https://travis-ci.com/pluf/http2.svg?branch=master)](https://travis-ci.com/pluf/http2)
+[![codecov](https://codecov.io/gh/pluf/http2/branch/master/graph/badge.svg)](https://codecov.io/gh/pluf/http2)
+[![Maintainability](https://api.codeclimate.com/v1/badges/513f356bdf26065cc009/maintainability)](https://codeclimate.com/github/pluf/http2/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/513f356bdf26065cc009/test_coverage)](https://codeclimate.com/github/pluf/http2/test_coverage)
+[![Coverage Status](https://coveralls.io/repos/github/pluf/http2/badge.svg?branch=master)](https://coveralls.io/github/pluf/http2?branch=master)
 
+This is a security module to block bots to access writable resourcess. 
 
 # Settings
 
--- tests/conf/config.php
+++ tests/conf/config.php
@@ -20,19 +20,11 @@ $cfg['url_format'] = 'simple';
 $cfg['tmp_folder'] = '/tmp';
 $cfg['upload_path'] = '/tmp';
 $cfg['middleware_classes'] = array(
+    '\Pluf\Middleware\Tenant',
     'Tenant_Middleware_ResourceAccess',
     '\Pluf\Captcha\Middleware\Verifier',
-    
-    'Pluf_Middleware_Session',
-    'User_Middleware_Session',
-    
-     // find tenant
-     // TODO: replace with tenant midleware
-     'Pluf_Middleware_TenantEmpty',
-     'Pluf_Middleware_TenantFromHeader',
-     'Pluf_Middleware_TenantFromDomain',
-     'Pluf_Middleware_TenantFromSubDomain', // It should be used only in multitenant state
-     'Pluf_Middleware_TenantFromConfig',
+    '\Pluf\Middleware\Session',
+    'User_Middleware_Session'
 );
 $cfg['secret_key'] = '5a8d7e0f2aad8bdab8f6eef725412850';
 
@@ -55,12 +47,11 @@ $cfg['log_delayed'] = false;
 $cfg['log_formater'] = '\Pluf\LoggerFormatter\Plain';
 $cfg['log_appender'] = '\Pluf\LoggerAppender\Console';
 
-
 // -------------------------------------------------------------------------
 // Tenants
 // -------------------------------------------------------------------------
 
-// multitenant
+$cfg['multitenant'] = false;
 
 // -------------------------------------------------------------------------
 // user
